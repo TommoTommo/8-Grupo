94fb737f1be2a5e40af883738b376d662ac688e7
TeresaMontana <tmontana@udesa.edu.ar>
2023-04-13 16:10:59 -0300

    sql

--- Data/zapatillas.sql
+++ Data/zapatillas.sql
@@ -12,3 +12,17 @@ CREATE TABLE usuarios(
     create_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     deleted_at timestamp NULL DEFAULT NULL
 );
+
+
+CREATE TABLE productos(
+    id int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+    nombre varchar(255) NOT NULL, 
+    descripcion varchar(255) NOT NULL, 
+    imagen varchar(255) NOT NULL, 
+    update_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    deleted_at timestamp NULL DEFAULT NULL, 
+
+    FkUserId INT(11) UNSIGNED NOT NULL, 
+    FOREIGN KEY (FkUserId) REFERENCES usuarios(id)
+);
