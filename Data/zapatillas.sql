f9e06da9e36dfc0fd7f46c699f3d1c7015167eb3
TeresaMontana <tmontana@udesa.edu.ar>
2023-04-13 16:27:15 -0300

    sql

--- Data/zapatillas.sql
+++ Data/zapatillas.sql
@@ -26,3 +26,18 @@ CREATE TABLE productos(
     FkUserId INT(11) UNSIGNED NOT NULL, 
     FOREIGN KEY (FkUserId) REFERENCES usuarios(id)
 );
+
+
+CREATE TABLE comentarios(
+    id int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+    comentarios varchar(255) NOT NULL, 
+    update_at timestamp NOT NULL DEFAULT CURRENT TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    create_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at timestamp NULL DEFAULT NULL, 
+
+    FkUserId INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY (FkUserId) REFERENCES usuarios(id),
+
+    FkProdId INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY (FkpRODid) REFERENCES productos(id)
+);
