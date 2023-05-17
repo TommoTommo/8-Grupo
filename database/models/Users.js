6fa2dae2edca3fb8788d05b347eaa4ad1a79e843
TeresaMontana <tmontana@udesa.edu.ar>
2023-05-17 19:53:37 -0300

    modelo Users

---
+++ database/models/Users.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, dataTypes) {
+    
+    let alias = "User";
+
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        name:{
+            type: dataTypes.STRING,
+        },
+        email:{
+            type: dataTypes.DECIMAL,
+        },
+        password:{
+            type: dataTypes.INTEGER,
+        },
+        profile:{
+            type: dataTypes.DATE,
+        },
+        date:{
+            type: dataTypes.INTEGER,
+        },
+        dni:{
+            type: dataTypes.INTEGER,
+        }
+    };
+
+    let config = {
+        tableName: 'users',
+        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
+        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
+    };
+
+    const users = sequelize.define(alias, cols, config);
+
+    return User;
+};
