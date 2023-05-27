eb7e159a00c3fb4ce41257dfcf2c8d6ab7f455b0
TommoTommo <tsteward@udesa.edu.ar>
2023-05-26 23:35:23 -0300

    homepage

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,13 @@ var router = express.Router();
 /* GET home page. */
 
 
-
+router.get('/', function(req, res, next) {
+    res.render('index');
+  });
 
 
 const indexController = require("../Controllers/indexController"); 
-/* GET users listing. */
-router.get('/', function(req, res) {
-    listanames=[];
-    listaimages=[];
-    for (let index = 0; index < 8; index++) {
-        
-        let name = Data.productos[index].id;
-        listanames.push(name);
-        let image=Data.productos[index].image;
-        listaimages.push(image)
-    }
-    });
+
 
 router.get('/results', indexController.results);
 
