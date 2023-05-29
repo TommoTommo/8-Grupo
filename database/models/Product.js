a6ea3a3c7cba6053aaca37b841c797c5ba1384bd
TeresaMontana <tmontana@udesa.edu.ar>
2023-05-29 17:43:47 -0300

    cambie modelos de productos

--- database/models/Product.js
+++ database/models/Product.js
@@ -11,16 +11,21 @@ module.exports = function(sequelize, dataTypes) {
         nombre:{
             type: dataTypes.STRING,
         },
+        descripcion:{
+            type: dataTypes.STRING, 
+        },
         imagen:{
             type: dataTypes.STRING,
         },
         user_id:{
 
             type: dataTypes.INTEGER,
+        }, 
+        create_at:{
+            type: dataTypes.DATE,
         },
-        descripcion:{
-
-            type: dataTypes.STRING,
+        update_at:{
+            type: dataTypes.DATE,
         }
 
 
@@ -37,8 +42,8 @@ module.exports = function(sequelize, dataTypes) {
 
     let config = {
         tableName: 'productos',
-        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
+        timestamps: true, //Si la tabla no tiene los campos created_at y updated_at
+        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
     };
 
     const product = sequelize.define(alias, cols, config);
