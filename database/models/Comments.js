f56b4797a008e852c0b0bd6278deb18ffa5bceca
TommoTommo <tsteward@udesa.edu.ar>
2023-05-29 17:42:46 -0300

    nueva db T

--- database/models/Comments.js
+++ database/models/Comments.js
@@ -11,18 +11,24 @@ module.exports = function(sequelize, dataTypes) {
         comentarios:{
             type: dataTypes.STRING,
         },
-        FkUserId:{
+        user_id:{
             type: dataTypes.INTEGER,
         },   
-        FkProdId:{
+        product_id:{
             type: dataTypes.INTEGER,
+        },
+        create_at:{
+            type: dataTypes.DATE,
+        },
+        update_at:{
+            type: dataTypes.DATE,
         }   
     };
     
 
     let config = {
         tableName: 'comentarios',
-        timestamps: true, //Si la tabla no tiene los campos created_at y updated_at
+        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
         underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
     };
 
